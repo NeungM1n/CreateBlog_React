4fe037cc22df3e4884eea9a1c62e1cc91aa2427c
dHyun <sessak0315@gmail.com>
2025-07-11 19:14:15 +0900

    34:43

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <section className="hero-wrapper">
+            <div className="paddings hero-container flexCenter hero-container">
+
+                {/* left side*/}
+                <div className="hero-left">
+                    <div className="hero-title">
+                        <h1>
+                            Discover <br />
+                            Most Suitable <br />
+                            Property
+                        </h1>
+                    </div>
+                </div>
+
+                {/* right side*/}
+
+                <div className="flexCenter hero-right">
+                    <div className="image-container">
+                        <img src="./hero-image.png" alt="" className="" />
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+export default Hero
