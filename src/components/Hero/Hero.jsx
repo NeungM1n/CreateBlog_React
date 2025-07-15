9cd4933d3bf802ef2f9dabef227761b9253e557a
dHyun <sessak0315@gmail.com>
2025-07-16 00:14:06 +0900

    until value

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Hero.css'
+import { HiLocationMarker } from 'react-icons/hi'
+import CountUp from 'react-countup'
 
 const Hero = () => {
     return (
@@ -9,11 +11,55 @@ const Hero = () => {
                 {/* left side*/}
                 <div className="hero-left">
                     <div className="hero-title">
+                        <div className="orange-circle" />
                         <h1>
                             Discover <br />
                             Most Suitable <br />
                             Property
                         </h1>
+                    </div>
+
+                    <div className="flexColStart hero-des">
+                        <span className='secondaryText'>Find a varitey 머시기</span>
+                        <span className='secondaryText'>dkssuddkssudclsrnemf</span>
+                    </div>
+
+                    <div className="flexCenter search-bar">
+                        <HiLocationMarker color="var(--blue)" size={25} />
+                        <input type="text" />
+                        <button className="button">Search</button>
+                    </div>
+
+                    <div className="flexCenter stats">
+                        <div className="flexColCenter stat">
+                            <span>
+                                <CountUp start={8800} end={9000} duration={4} />
+                                <span>+</span>
+                            </span>
+                            <span className='secondaryText'>
+                                Premium Products
+                            </span>
+                        </div>
+
+                        <div className="flexColCenter stat">
+                            <span>
+                                <CountUp start={1950} end={2000} duration={4} />
+                                <span>+</span>
+                            </span>
+                            <span className='secondaryText'>
+                                Happy Customers
+                            </span>
+                        </div>
+
+                        <div className="flexColCenter stat">
+                            <span>
+                                <CountUp end={28} />
+                                <span>+</span>
+                            </span>
+                            <span className='secondaryText'>
+                                Award Winning
+                            </span>
+                        </div>
                     </div>
                 </div>
 
